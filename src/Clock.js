30e7297b45a9dc394c32db2ecd855f123f23eb0a
beendra <alexandra.ricardel@gmail.com>
2021-04-10 13:05:54 -0500

    commit tree 56a1d2b28a7b8607f78fdb2c6f21bba7ccba5987

--- src/Clock.js
+++ src/Clock.js
@@ -13,3 +13,22 @@ function Clock() {
 }
 
 export default Clock;
+
+//with clean up function: 
+
+// function Clock() {
+//   const [time, setTime] = useState(new Date());
+
+//   useEffect(() => {
+//     const timerID = setInterval(() => {
+//       setTime(new Date());
+//     }, 1000);
+
+//     // returning a cleanup function
+//     return function cleanup() {
+//       clearInterval(timerID);
+//     };
+//   }, []);
+
+//   return <div>{time.toString()}</div>;
+// } 
